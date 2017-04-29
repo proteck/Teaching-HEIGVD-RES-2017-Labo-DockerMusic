eaddbedd83d05493def4447329cf455c0a066ab5
Maxime Guillod <maxime@proteck.ch>
2017-04-29 18:24:19 +0200

    Add clean

--- docker/image-auditor/src/auditor.js
+++ docker/image-auditor/src/auditor.js
@@ -1,6 +1,8 @@
 const IP = "239.42.42.42"
 const CAST_PORT = 12345
 const SRV_PORT = 2205
+const INTERVAL_CHECK = 1000
+const INTERVAL_KEEP = 5000
 
 var dgram = require("dgram")
 var net = require("net")
@@ -9,15 +11,15 @@ var multiCast = dgram.createSocket("udp4")
 
 var musiciansJson = []
 
-multiCast.on("message", function(msg, rinfo) {
+multiCast.on("message", function (msg, rinfo) {
     console.log(" <<< " + msg)
     var json = JSON.parse(msg)
 
     // On test si il est deja dans la liste
-    for(var i = 0; i < musiciansJson.length; i++) {
-        if(json.uuid == musiciansJson[i].uuid) {
+    for (var i = 0; i < musiciansJson.length; i++) {
+        if (json.uuid == musiciansJson[i].uuid) {
             musiciansJson[i].time = new Date();
-            return 
+            return
         }
     }
     // il est pas encore dans la liste, on l'ajoute
@@ -25,10 +27,24 @@ multiCast.on("message", function(msg, rinfo) {
     musiciansJson.push(json)
 })
 
-multiCast.bind(CAST_PORT, function() {
+multiCast.bind(CAST_PORT, function () {
     multiCast.addMembership(IP)
 })
 
 var srv = net.createServer(function (socket) {
     socket.end(JSON.stringify(musiciansJson))
 }).listen(SRV_PORT)
+
+function clean() {
+    console.log(" - Try clean")
+    var actualTime = new Date()
+
+    for (var i = 0; i < musiciansJson.length; i++) {
+        if (actualTime - musiciansJson[i].time > INTERVAL_KEEP) {
+            console.log("remove: " + JSON.stringify(musiciansJson.splice))
+            musiciansJson.splice(i, 1)
+        }
+    }
+}
+
+setInterval(clean, INTERVAL_CHECK)
